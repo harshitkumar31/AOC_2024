d0df6cbc71458455843d762783fea83a299351e5
Harshit Kumar <15359962+harshitkumar31@users.noreply.github.com>
2024-12-03 15:47:35 +0530

    AOC_2024_2_1

---
+++ aoc_2/aoc_2_2/src/main.rs
@@ -0,0 +1,69 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+    let contents = fs::read_to_string(file_path).expect("Expected input");
+
+    let contents_arr: Vec<&str> = contents.split("\n").collect();
+    let mut result = 0;
+    for line in contents_arr {
+        let line_arr: Vec<&str> = line.split(" ").collect();
+        let mut prev = -1;
+        let mut prevTrend = 0; // 1 is increasing, -1 is decreasing
+        let mut safe = true;
+        let mut tolerated = 0;
+        for num in line_arr {
+            let num = num.parse::<i32>().unwrap();
+            if prev == -1 {
+                prev = num;
+                continue;
+            }
+            let diff = num.abs_diff(prev);
+            if !(diff >= 1 && diff <= 3) {
+                tolerated += 1;
+                if tolerated > 1 {
+                    safe = false;
+                    break;
+                } else {
+                    continue;
+                }
+            }
+            if num > prev {
+                if prevTrend == -1 {
+                    tolerated += 1;
+                    if tolerated > 1 {
+                        safe = false;
+                        break;
+                    } else {
+                        continue;
+                    }
+                } else {
+                    prevTrend = 1;
+                }
+            }
+            if num < prev {
+                if prevTrend == 1 {
+                    tolerated += 1;
+
+                    if tolerated > 1 {
+                        safe = false;
+                        break;
+                    } else {
+                        continue;
+                    }
+                } else {
+                    prevTrend = -1;
+                }
+            }
+
+            prev = num;
+        }
+        if safe {
+            result += 1;
+        }
+    }
+    println!("Result is {result}");
+}
