16aee80005ef80d91b76f31ca9adb7e15229ab22
Harshit Kumar <15359962+harshitkumar31@users.noreply.github.com>
2024-12-03 14:59:25 +0530

    AOC_2024_2_1

--- aoc_2/aoc_2_1/src/main.rs
+++ aoc_2/aoc_2_1/src/main.rs
@@ -1,3 +1,52 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+    let contents = fs::read_to_string(file_path).expect("Expected input");
+
+    let contents_arr: Vec<&str> = contents.split("\n").collect();
+    let mut result = 0;
+    for line in contents_arr {
+        let line_arr: Vec<&str> = line.split(" ").collect();
+        let mut prev = -1;
+        let mut prevTrend = 0; // 1 is increasing, -1 is decreasing
+        let mut safe = true;
+        for num in line_arr {
+            let num = num.parse::<i32>().unwrap();
+            if prev == -1 {
+                prev = num;
+                continue;
+            }
+            let diff = num.abs_diff(prev);
+            if !(diff >= 1 && diff <= 3) {
+                safe = false;
+                break;
+            }
+            if num > prev {
+                if prevTrend == -1 {
+                    safe = false;
+                    break;
+                } else {
+                    prevTrend = 1;
+                }
+            }
+            if num < prev {
+                if prevTrend == 1 {
+                    safe = false;
+                    break;
+                } else {
+                    prevTrend = -1;
+                }
+            }
+
+            prev = num;
+        }
+        if safe {
+            result += 1;
+        }
+    }
+    println!("Result is {result}");
 }
