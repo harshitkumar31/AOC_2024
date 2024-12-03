d8df22b67e857170329ced66ddd18982af5f7d02
Harshit Kumar <15359962+harshitkumar31@users.noreply.github.com>
2024-12-03 16:27:21 +0530

    AOC_2024_2_2

--- aoc_2/aoc_2_1/src/main.rs
+++ aoc_2/aoc_2_1/src/main.rs
@@ -1,6 +1,43 @@
 use std::env;
 use std::fs;
 
+fn validate(input: Vec<&str>) -> bool {
+    let mut prev = -1;
+    let mut prevTrend = 0; // 1 is increasing, -1 is decreasing
+    let mut safe = true;
+    for num in input {
+        let num = num.parse::<i32>().unwrap();
+        if prev == -1 {
+            prev = num;
+            continue;
+        }
+        let diff = num.abs_diff(prev);
+        if !(diff >= 1 && diff <= 3) {
+            safe = false;
+            break;
+        }
+        if num > prev {
+            if prevTrend == -1 {
+                safe = false;
+                break;
+            } else {
+                prevTrend = 1;
+            }
+        }
+        if num < prev {
+            if prevTrend == 1 {
+                safe = false;
+                break;
+            } else {
+                prevTrend = -1;
+            }
+        }
+
+        prev = num;
+    }
+    return safe;
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -11,40 +48,8 @@ fn main() {
     let mut result = 0;
     for line in contents_arr {
         let line_arr: Vec<&str> = line.split(" ").collect();
-        let mut prev = -1;
-        let mut prevTrend = 0; // 1 is increasing, -1 is decreasing
-        let mut safe = true;
-        for num in line_arr {
-            let num = num.parse::<i32>().unwrap();
-            if prev == -1 {
-                prev = num;
-                continue;
-            }
-            let diff = num.abs_diff(prev);
-            if !(diff >= 1 && diff <= 3) {
-                safe = false;
-                break;
-            }
-            if num > prev {
-                if prevTrend == -1 {
-                    safe = false;
-                    break;
-                } else {
-                    prevTrend = 1;
-                }
-            }
-            if num < prev {
-                if prevTrend == 1 {
-                    safe = false;
-                    break;
-                } else {
-                    prevTrend = -1;
-                }
-            }
 
-            prev = num;
-        }
-        if safe {
+        if validate(line_arr) {
             result += 1;
         }
     }
--- aoc_2/aoc_2_2/src/main.rs
+++ aoc_2/aoc_2_2/src/main.rs
@@ -1,6 +1,58 @@
 use std::env;
 use std::fs;
 
+fn validate(input: Vec<&str>) -> bool {
+    let mut prev = -1;
+    let mut prevTrend = 0; // 1 is increasing, -1 is decreasing
+    let mut safe = true;
+    for num in input {
+        let num = num.parse::<i32>().unwrap();
+        if prev == -1 {
+            prev = num;
+            continue;
+        }
+        let diff = num.abs_diff(prev);
+        if !(diff >= 1 && diff <= 3) {
+            safe = false;
+            break;
+        }
+        if num > prev {
+            if prevTrend == -1 {
+                safe = false;
+                break;
+            } else {
+                prevTrend = 1;
+            }
+        }
+        if num < prev {
+            if prevTrend == 1 {
+                safe = false;
+                break;
+            } else {
+                prevTrend = -1;
+            }
+        }
+
+        prev = num;
+    }
+    return safe;
+}
+
+fn validateByRemoval(input: Vec<&str>) -> bool {
+    let mut i = 0;
+    while i < input.len() {
+        let new_input_st = &input[0..i];
+        let new_input_end = &input[i + 1..];
+        let new_input = [new_input_st, new_input_end].concat();
+        if validate(new_input) {
+            return true;
+        }
+        i += 1;
+    }
+
+    return false;
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -11,57 +63,7 @@ fn main() {
     let mut result = 0;
     for line in contents_arr {
         let line_arr: Vec<&str> = line.split(" ").collect();
-        let mut prev = -1;
-        let mut prevTrend = 0; // 1 is increasing, -1 is decreasing
-        let mut safe = true;
-        let mut tolerated = 0;
-        for num in line_arr {
-            let num = num.parse::<i32>().unwrap();
-            if prev == -1 {
-                prev = num;
-                continue;
-            }
-            let diff = num.abs_diff(prev);
-            if !(diff >= 1 && diff <= 3) {
-                tolerated += 1;
-                if tolerated > 1 {
-                    safe = false;
-                    break;
-                } else {
-                    continue;
-                }
-            }
-            if num > prev {
-                if prevTrend == -1 {
-                    tolerated += 1;
-                    if tolerated > 1 {
-                        safe = false;
-                        break;
-                    } else {
-                        continue;
-                    }
-                } else {
-                    prevTrend = 1;
-                }
-            }
-            if num < prev {
-                if prevTrend == 1 {
-                    tolerated += 1;
-
-                    if tolerated > 1 {
-                        safe = false;
-                        break;
-                    } else {
-                        continue;
-                    }
-                } else {
-                    prevTrend = -1;
-                }
-            }
-
-            prev = num;
-        }
-        if safe {
+        if validate(line_arr.clone()) || validateByRemoval(line_arr) {
             result += 1;
         }
     }
